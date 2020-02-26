65696693734cb9c9e3f883d830982a01d3e33927
Zhaoyu <weixiaobo88@gmail.com>
2020-02-26 11:09:23 +0800

    refactor: extract the conversions

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,12 +1,14 @@
+import java.util.HashMap;
+
 public class Length {
     private final double value;
     private final String unit;
     private static final String FOOT = "foot";
     private static final String YARD = "yard";
     private static final String INCH = "inch";
-    private static final int YARD_TO_FOOT = 3;
-    private static final int FOOT_TO_INCH = 12;
-    private static final int YARD_TO_INCH = 36;
+    private static final double YARD_TO_FOOT = 3;
+    private static final double FOOT_TO_INCH = 12;
+    private static final double YARD_TO_INCH = 36;
     private double FOOT_TO_YARD = 1.0 / YARD_TO_FOOT;
     private double INCH_TO_FOOT = 1.0 / FOOT_TO_INCH;
     private double INCH_TO_YARD = 1.0 / YARD_TO_INCH;
@@ -17,34 +19,7 @@ public class Length {
     }
 
     public Length as(String unit) {
-        Length length = this;
-
-        if (this.unit.equals(FOOT)) {
-            if (unit.equals(YARD)) {
-                length = new Length(this.value * FOOT_TO_YARD, unit);
-            } else if (unit.equals(INCH)) {
-                length = new Length(this.value * FOOT_TO_INCH, unit);
-            }
-
-        }
-
-        if (this.unit.equals(YARD)) {
-            if (unit.equals(INCH)) {
-                length = new Length(this.value * YARD_TO_INCH, unit);
-            } else if (unit.equals(FOOT)){
-                length = new Length(this.value * YARD_TO_FOOT, unit);
-            }
-        }
-
-        if (this.unit.equals(INCH)) {
-            if (unit.equals(FOOT)) {
-                length = new Length(this.value * INCH_TO_FOOT, unit);
-            } else if (unit.equals(YARD)) {
-                length = new Length(this.value * INCH_TO_YARD, unit);
-            }
-        }
-
-        return length;
+        return new Length(this.value * getConversion(this.unit, unit), unit);
     }
 
     public double getVal() {
@@ -53,5 +28,30 @@ public class Length {
 
     public String getUnit() {
         return this.unit;
+    }
+
+    private double getConversion(String from, String to) {
+        HashMap<String, Double> conversions = new HashMap<>();
+        double SAME_RATIO = 1.0;
+
+        if (from.equals(FOOT)) {
+            conversions.put(FOOT, SAME_RATIO);
+            conversions.put(YARD, FOOT_TO_YARD);
+            conversions.put(INCH, FOOT_TO_INCH);
+        }
+
+        if (from.equals(YARD)) {
+            conversions.put(YARD, SAME_RATIO);
+            conversions.put(FOOT, YARD_TO_FOOT);
+            conversions.put(INCH, YARD_TO_INCH);
+        }
+
+        if (from.equals(INCH)) {
+            conversions.put(INCH, SAME_RATIO);
+            conversions.put(FOOT, INCH_TO_FOOT);
+            conversions.put(YARD, INCH_TO_YARD);
+        }
+
+        return conversions.get(to);
     }
 }
