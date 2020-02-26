d2c6816e1b797ab94aba5a1346669b6bc7a10184
Zhaoyu <weixiaobo88@gmail.com>
2020-02-26 15:15:16 +0800

    refactor: create unit enum

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,46 +1,55 @@
 public class Length {
+    public static final String FOOT = "f";
+    public static final String YARD = "yard";
+    public static final String INCH = "inch";
     private final double value;
     private final String unit;
 
-    public Length(double val, String uinnt) {
-        this.value = val;
-        this.unit = uinnt;
+    public Length(double value, String unit) {
+        this.value = value;
+        this.unit = unit;
     }
 
-    public Length as(String u) {
-        Length len = this;
-        if (this.unit.equals("f")) {
-            if (u.equals("yard")) {
-                len = new Length(this.value / 3, u);
-            } else if (u.equals("inch")) {
-                len = new Length(this.value * 12, u);
-            }
-        }
-
-        if (this.unit.equals("yard")) {
-            if (u.equals("inch")) {
-                len = new Length(this.value * 36, u);
-            } else if (u.equals("f")){
-                len = new Length(this.value * 3, u);
-            }
-        }
-
-        if (this.unit.equals("inch")) {
-            if (u.equals("f")) {
-                len = new Length(this.value / 12, u);
-            } else if (u.equals("yard")) {
-                len = new Length(this.value / 36, u);
-            }
-        }
-
-        return len;
+    public Length as(String unit) {
+        Unit unit_temp = unit.equals(Unit.FOOT) ? Unit.FOOT : null;
+        return this.as_temp(unit, unit_temp);
     }
 
-    public double getVal() {
+    public Length as_temp(String unit, Unit unit_temp) {
+        Length length = this;
+        String foot = Length.FOOT;
+        if (this.unit.equals(foot)) {
+            if (unit.equals(Length.YARD)) {
+                length = new Length(this.value / 3, unit);
+            } else if (unit.equals(Length.INCH)) {
+                length = new Length(this.value * 12, unit);
+            }
+        }
+
+        if (this.unit.equals(Length.YARD)) {
+            if (unit.equals(Length.INCH)) {
+                length = new Length(this.value * 36, unit);
+            } else if (unit.equals(foot)) {
+                length = new Length(this.value * 3, unit);
+            }
+        }
+
+        if (this.unit.equals(Length.INCH)) {
+            if (unit.equals(foot)) {
+                length = new Length(this.value / 12, unit);
+            } else if (unit.equals(Length.YARD)) {
+                length = new Length(this.value / 36, unit);
+            }
+        }
+
+        return length;
+    }
+
+    public double getValue() {
         return this.value;
     }
 
-    public String getUinnt() {
+    public String getUnit() {
         return this.unit;
     }
 }
