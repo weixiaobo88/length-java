524e1b937251c0fb0bbc512c5a8597a5b87ab746
Zhaoyu <weixiaobo88@gmail.com>
2020-02-27 10:55:26 +0800

    refactor: remove useless variables and methods

--- src/main/java/Length.java
+++ src/main/java/Length.java
@@ -1,14 +1,9 @@
 public class Length {
-    public static final String FOOT = "f";
-    public static final String YARD = "yard";
-    public static final String INCH = "inch";
     private final double value;
-    private final String unit;
     private Unit temp_unit;
 
-    public Length(double value, String unit, Unit temp_unit) {
+    public Length(double value, Unit temp_unit) {
         this.value = value;
-        this.unit = unit;
         this.temp_unit = temp_unit;
     }
 
@@ -16,25 +11,25 @@ public class Length {
         Length length = this;
         if (this.temp_unit == Unit.FOOT) {
             if (temp_unit == Unit.YARD) {
-                length = new Length(this.value / 3, null, temp_unit);
+                length = new Length(this.value / 3, temp_unit);
             } else if (temp_unit == Unit.INCH) {
-                length = new Length(this.value * 12, null, temp_unit);
+                length = new Length(this.value * 12, temp_unit);
             }
         }
 
         if (this.temp_unit == Unit.YARD) {
             if (temp_unit == Unit.INCH) {
-                length = new Length(this.value * 36, null, temp_unit);
+                length = new Length(this.value * 36, temp_unit);
             } else if (temp_unit == Unit.FOOT) {
-                length = new Length(this.value * 3, null, temp_unit);
+                length = new Length(this.value * 3, temp_unit);
             }
         }
 
         if (this.temp_unit == Unit.INCH) {
             if (temp_unit == Unit.FOOT) {
-                length = new Length(this.value / 12, null, temp_unit);
+                length = new Length(this.value / 12, temp_unit);
             } else if (temp_unit == Unit.YARD) {
-                length = new Length(this.value / 36, null, temp_unit);
+                length = new Length(this.value / 36, temp_unit);
             }
         }
 
@@ -43,10 +38,6 @@ public class Length {
 
     public double getValue() {
         return this.value;
-    }
-
-    public String getUnit() {
-        return this.unit;
     }
 
     public Unit getTemp_unit() {
